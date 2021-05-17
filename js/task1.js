5c85f7aedc63595a7474b43d16980231d367315d
Yuliia Bondarenko <bondarenko047@gmail.com>
2021-05-17 12:06:38 +0300

    task1 done

--- js/task1.js
+++ js/task1.js
@@ -14,3 +14,15 @@
 // delay(1500).then(logger); // Resolved after 1500ms
 
 // --- РЕШЕНИЕ ---
+ 
+const delay = ms => {
+    return new Promise(resolved => {
+    setTimeout(resolved(ms));
+    })
+};
+
+const logger = time => console.log(`Resolved after ${time} ms`);
+
+delay(2000).then(logger); // Resolved after 2000ms
+delay(1000).then(logger); // Resolved after 1000ms
+delay(1500).then(logger); // Resolved after 1500ms
